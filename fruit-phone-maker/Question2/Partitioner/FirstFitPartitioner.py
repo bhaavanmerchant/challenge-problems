c011b774b10576bd87680036d8fe3294ba2e3b6d
Bhaavan Merchant <bhaavan.merchant@booking.com>
2020-03-03 12:20:56 +0100

    Add tests and docstring to q2

--- fruit-phone-maker/Question2/Partitioner/FirstFitPartitioner.py
+++ fruit-phone-maker/Question2/Partitioner/FirstFitPartitioner.py
@@ -4,34 +4,46 @@ import collections
 
 class FirstFitPartitioner:
     def __init__(self, K: int, counts: List[Tuple[int, str]]):
+        """
+        Initializer for the FirstFitPartitioner, which constructs a map of partitioner strings, and assigns them a parition id
+
+        Keyword argument:
+        K: number of partitions available
+        counts: A list of entries of counts of partitioning column string. When the column is user_id for example, it will be (counts_seen_of_a_user_id, user_id).
+        """
+
         total_size = sum([c[0] for c in counts])
         avg_bin_size = total_size / K # Since we assume an exponential distribution, f(x) = lambda * e ** (- lambda * x), we can assume most values will be less that the average value
-
-        target_bin_size = avg_bin_size * (1 + 1/K**1.2) # Applying a smoothening function, to allow misfitting for smaller K values
 
         self.partitionAssignment = {}
         partitionSize = [0] * K
 
         counts.sort(key=lambda k: k[0], reverse=True)
-        i = 0
-        for count, partitionStr in counts:
+        i = 0                   # Counter to attempt to fit in the given number in a given partition id
+        for count, partitionColumnStr in counts:
             jumps = 0
-            while count + partitionSize[i] > target_bin_size and jumps < K:
+            while count + partitionSize[i] > avg_bin_size and jumps < K:
                 i += 1
-                jumps += 1
-                i = i % K
-            self.partitionAssignment[partitionStr] = i
+                jumps += 1      # Increment the total number of jumps made to fit in the given value
+                i = i % K       # Once the i has exceeded the number of available partitions, we restart from the 1st partition
+            self.partitionAssignment[partitionColumnStr] = i
             partitionSize[i] += count
 
         self.partitionSize = partitionSize
 
-    def getPartitionId(self, partitionStr: str) -> int:
-        if partitionStr in self.partitionAssignment:
-            return self.partitionAssignment[partitionStr]
-        return hash(partitionStr) % len(self.partitionSize)
+    def getPartitionId(self, partitionColumnStr: str) -> int:
+        """
+        At query time, this function will return a partition id for a partitioning column string. For example, user_id.
+        """
+        if partitionColumnStr in self.partitionAssignment: # Fetch an assigned partition number for the partitoner string value
+            return self.partitionAssignment[partitionColumnStr]
+        # Handle a case when we may be asked partition id for a partition string we haven't seen before.
+        # This is a rare case, but may occur if there was a change in data between calling class constructor and the getPartitionId method
+        # In this case, we naively return a random partition id.
+        return hash(partitionColumnStr) % len(self.partitionSize)
 
-    def _getBinLoad(self):
+    def getLoadFactor(self):
+        """Get a measure of how populated the partitions are assigned, relative to the size of the maximum sized partition. If all partitions are exactly equal, this should be 1"""
         biggestBin = max(self.partitionSize)
         occupancyFactor = sum(self.partitionSize) / (biggestBin * len(self.partitionSize))
-
         return occupancyFactor
--- fruit-phone-maker/Question2/tests/test_FirstFitPartitioner.py
+++ fruit-phone-maker/Question2/tests/test_FirstFitPartitioner.py
@@ -8,12 +8,16 @@ class TestFirstFitPartitioner(unittest.TestCase):
     def test_smallData(self):
         counts = [(6,'a'), (3, 'b'), (2, 'c'), (1, 'd'), (4,'f')]
         partitioner = FirstFitPartitioner(3, counts)
-        self.assertGreaterEqual(partitioner._getBinLoad(), 0.8)
+        partitionSize = partitioner.partitionSize
+        partitionSize.sort()
+        self.assertEqual(partitionSize, [5, 5, 6])
 
     def test_unbalancedData(self):
         counts = [(6,'a'), (6, 'b'), (6, 'c'), (6, 'd')]
         partitioner = FirstFitPartitioner(3, counts)
-        self.assertGreaterEqual(partitioner._getBinLoad(), 0.8)
+        partitionSize = partitioner.partitionSize
+        partitionSize.sort()
+        self.assertEqual(partitionSize, [6, 6, 12])
 
     def test_bigData(self):
         size_big = 1_000_000
@@ -22,6 +26,27 @@ class TestFirstFitPartitioner(unittest.TestCase):
         for i in range(size_big):
             ids[i] = str(uuid.uuid4())[:8]
         counts = list(zip(exp_dist, ids))
-        print("solving")
-        partitioner = FirstFitPartitioner(10000, counts)
-        print(partitioner._getBinLoad())
+        partitioner = FirstFitPartitioner(10_000, counts)
+        self.assertGreaterEqual(partitioner.getLoadFactor(), 0.99)
+
+    def test_known_getPartitionId(self):
+        counts = [(5,'a'), (3, 'b'), (2, 'c'), (1, 'd'), (4,'f')]
+        partitioner = FirstFitPartitioner(3, counts)
+        # In a firstfit partitioner, the first element will be assigned to the 1st partition (which is 0 in this case)
+        self.assertEqual(partitioner.getPartitionId('a'),0)
+
+    def test_unknown_getPartitionId(self):
+        counts = [(5,'a'), (3, 'b'), (2, 'c'), (1, 'd'), (4,'f')]
+        partitioner = FirstFitPartitioner(3, counts)
+        # For an unknown partition query we still return a partition id, within the given partition sizes
+        self.assertLess(partitioner.getPartitionId('q'), len(partitioner.partitionSize))
+
+    def test_balanced_getLoadFactor(self):
+        counts = [(5,'a'), (3, 'b'), (2, 'c'), (1, 'd'), (4,'f')]
+        partitioner = FirstFitPartitioner(3, counts)
+        self.assertEqual(partitioner.getLoadFactor(), 1)
+
+    def test_unbalanced_getLoadFactor(self):
+        counts = [(6,'a'), (6, 'b'), (6, 'c'), (6, 'd')]
+        partitioner = FirstFitPartitioner(3, counts)
+        self.assertEqual(partitioner.getLoadFactor(), 2/3)
